2436925f7f6990b2d3d691f036ce28c4cb3a93e5
Rafli Malik <Rafli.Malik@gmail.com>
2024-09-17 11:25:03 +0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Font Calculator</title>
+	<meta
+		name="description"
+		content="A simple tool to calculate font sizes based on a modular scale."
+	/>
+</svelte:head>
+
+<slot />
