4af5ecd515163fb2a463c26fcae0192abbdcda4c
Rafli Malik <Rafli.Malik@gmail.com>
2024-10-30 13:39:42 +0700

    chore(deps): add plausible

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,11 @@
 		content="A simple tool to calculate font sizes based on a modular scale."
 	/>
 	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet" />
+	<script
+		defer
+		data-domain="raflimalik.com"
+		src="https://plausible.io/js/script.file-downloads.hash.outbound-links.js"
+	></script>
 </svelte:head>
 
 <slot />
