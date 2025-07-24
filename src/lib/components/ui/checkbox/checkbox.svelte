1000f89a98a5207e65720a095dd63c9b5a79b7b1
Rafli Malik <Rafli.Malik@gmail.com>
2025-07-24 12:14:18 +0700

    chore: update component

--- src/lib/components/ui/checkbox/checkbox.svelte
+++ src/lib/components/ui/checkbox/checkbox.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { Checkbox as CheckboxPrimitive } from "bits-ui";
-
 	import CheckIcon from "@lucide/svelte/icons/check";
 	import MinusIcon from "@lucide/svelte/icons/minus";
 	import { cn, type WithoutChildrenOrChild } from "$lib/utils.js";
